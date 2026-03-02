45045e944e21aed8e9abe7e51a9c2658891e3a14
KiHyeonLee <kihyeon11217780@gmail.com>
2026-03-02 21:27:02 +0900

    Fix: 깃허브 페이지 배포 경로 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/elephants_volunteermate/',
+    base: '/elephants_volunteermate/', // 깃허브 저장소 이름을 경로로 설정
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
